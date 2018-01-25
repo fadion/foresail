abcbf646cc9b234230f215ee6b42860c4412541c
Fadion Dashi <jonidashi@gmail.com>
2018-01-25 08:31:42 +0100

    Make colors in project settings as circles
    Signed-off-by: Fadion Dashi <jonidashi@gmail.com>

--- src/renderer/views/projects/Settings.vue
+++ src/renderer/views/projects/Settings.vue
@@ -8,11 +8,7 @@
     data() {
       return {
         newProject: Object.assign({}, this.project),
-        // Divide the colors into two colums.
-        colors: {
-          first: config.colors.slice(0, Math.round(config.colors.length / 2)),
-          second: config.colors.slice(Math.round(config.colors.length / 2))
-        }
+        colors: config.colors
       }
     },
 
@@ -63,13 +59,8 @@
 
       <label class="form-section form-section--separator">
         <span class="form-label">Color</span>
-        <div class="projectSettings-colors">
-          <div>
-            <input type="radio" v-for="color in colors.first" class="form-color" name="color" :value="color" :style="{ background: color }" v-model="newProject.color">
-          </div>
-          <div>
-            <input type="radio" v-for="color in colors.second" class="form-color" name="color" :value="color" :style="{ background: color }" v-model="newProject.color">
-          </div>
+        <div>
+          <input type="radio" v-for="color in colors" class="form-color" name="color" :value="color" :style="{ background: color }" v-model="newProject.color">
         </div>
       </label>
 
@@ -101,22 +92,16 @@
       font-weight: normal;
     }
 
+    .form-color {
+      margin: 0 5px 5px 0;
+    }
+
     &-enter-active, &-leave-active {
       transition: right .4s ease-out;
     }
 
     &-enter, &-leave-to {
       right: -400px;
-    }
-  }
-
-  .projectSettings-colors {
-    position: relative;
-    display: flex;
-    justify-content: space-between;
-
-    div {
-      width: 48%;
     }
   }
 </style>
